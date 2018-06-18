d9c1dc22d46d735bad90f8b169abeb0d639b8c12
slowbeam <sedwardscode@gmail.com>
2018-06-18 11:27:41 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    f_name = self.first_name + " " + self.last_name
+    f_name
+  end
+
+  def list_roles
+    Character.all.where("actor_id =?", self.id).map do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+
+  end
 end
---
+++ db/migrate/008_add_day_to_shows.rb
@@ -0,0 +1,6 @@
+class AddDayToShows < ActiveRecord::Migration[5.1]
+  def change
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+  end
+end
